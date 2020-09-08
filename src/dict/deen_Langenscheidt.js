4e6737b8f37b8b12b91a5ff1b9d81b8bfb435cc9
cedarhillgoods <admin@cedarhillgoods.com>
2020-09-08 00:20:33 -0700

    Update deen_Langenscheidt.js

--- src/dict/deen_Langenscheidt.js
+++ src/dict/deen_Langenscheidt.js
@@ -129,7 +129,7 @@ class deen_Langenscheidt {
         //   });
         // }
 
-        let x = doc.querySelectorAll('.additional-entry');
+        let x = doc.getElementsByClassName('additional-entry');
         for(let i=0; i<x.length; i++)
         {
           x[i].insertAdjacentHTML('afterend', '<br />');
