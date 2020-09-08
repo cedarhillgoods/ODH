64bb8a3e054b80633c94c6e50ac41d9ffb86c595
cedarhillgoods <admin@cedarhillgoods.com>
2020-09-08 00:55:40 -0700

    Update deen_Langenscheidt.js

--- src/dict/deen_Langenscheidt.js
+++ src/dict/deen_Langenscheidt.js
@@ -168,9 +168,9 @@ class deen_Langenscheidt {
     renderCSS() {
         let css = `
             <style>
-                // input {
-                //     display: none;
-                // }
+                input {
+                    display: none;
+                }
             </style>`;
 
         return css;
